b4bc94877e6c7c72b102a4628ea01ba7f9bfd9b4
Rajkumar Paladugu <rpaladugu@adaequare.com>
2020-10-01 02:46:32 +0530

    Dashboard integration

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to medic-app-v7!');
+    expect(page.getTitleText()).toEqual('Welcome to itap-v7!');
   });
 });
